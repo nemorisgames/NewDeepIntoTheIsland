c64c6c7c7448c11f30ec7f76f2a95be64278eff5
nemorisgames <contacto@nemorisgames.com>
2018-07-22 19:34:34 -0400

    Witcher update 2

---
+++ NewDeepIntoTheIsland/Assets/Scripts/Witcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Witcher : MonoBehaviour {
+    public Transform target;
+    Animator animator;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        // Cache agent component and destination
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("InputVertical", agent.speed * 0.2f);
+        // Update destination if the target moves one unit
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            agent.destination = destination;
+        }
+    }
+}
