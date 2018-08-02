bde2781e22ae7e7d6073c7daccfe668d808d295d
njaramillo-ng <njaramillo@nemorisgames.com>
2018-08-02 18:07:51 -0400

    Lock

--- NewDeepIntoTheIsland/Assets/Scripts/Misc/Lock.cs
+++ NewDeepIntoTheIsland/Assets/Scripts/Misc/Lock.cs
@@ -18,7 +18,7 @@ public class Lock : MonoBehaviour {
         if (rotating) return;
         rotating = true;
         Keys[index].tweenRotation.from = Keys[index].tweenRotation.to;
-        Keys[index].tweenRotation.to = new Vector3(Keys[index].tweenRotation.from.x + 36f, 0f, 0f);
+        Keys[index].tweenRotation.to = new Vector3(0f, 0f, Keys[index].tweenRotation.from.z + 36f);
         Keys[index].tweenRotation.ResetToBeginning();
         Keys[index].tweenRotation.PlayForward();
         Keys[index].currentIndex = (Keys[index].currentIndex+1)%10;
@@ -27,11 +27,12 @@ public class Lock : MonoBehaviour {
     public void RotationFinished()
     {
         rotating = false;
+        CheckLock();
     }
 	
 	void LateUpdate () {
         if (opened) return;
-        if (Input.GetKeyDown(KeyCode.E) && !CellPhone.phoneUp)
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))&& !CellPhone.phoneUp)
         {
             // Get mask for the raycast
             int layerMask = LayerMask.GetMask("Interactable");
@@ -53,13 +54,17 @@ public class Lock : MonoBehaviour {
                 {
                     if(hit.transform.gameObject == buttonDone)
                     {
-                        opened = CheckLockCombination();
-                        if (opened && objectToUnlock != null) objectToUnlock.Unlock();
+                        CheckLock();
                     }
                 }
             }
         }
 	}
+
+    void CheckLock(){
+        opened = CheckLockCombination();
+        if (opened && objectToUnlock != null) objectToUnlock.Unlock();
+    }
 
     bool CheckLockCombination()
     {
